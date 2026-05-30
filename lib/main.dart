c46c89588d7cc01cb5f8673652e4ecf1af767bc7
Kamel Assakri <matrix332008@gmail.com>
2026-05-30 15:00:54 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,10 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
-          image: DecorationImage(image: AssetImage("assets/bg.jpg"), fit: BoxFit.cover),
+          image: DecorationImage(
+            image: AssetImage("assets/background.jpeg"), // اسم صحيح
+            fit: BoxFit.cover,
+          ),
         ),
         child: const Center(
           child: Column(
@@ -101,7 +104,6 @@ class _LoginScreenState extends State<LoginScreen> {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     
     if (isXtream) {
-      // Xtream Codes API
       try {
         final response = await http.get(Uri.parse(
           '${url.text}/player_api.php?username=${user.text}&password=${pass.text}'
@@ -123,7 +125,6 @@ class _LoginScreenState extends State<LoginScreen> {
         _showError('مشكل في الاتصال بالسيرفر');
       }
     } else {
-      // M3U
       await prefs.setBool('isLogged', true);
       await prefs.setString('m3uUrl', url.text);
       await prefs.setBool('isXtream', false);
@@ -141,7 +142,10 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
-          image: DecorationImage(image: AssetImage("assets/bg.jpg"), fit: BoxFit.cover),
+          image: DecorationImage(
+            image: AssetImage("assets/background.jpeg"), // اسم صحيح
+            fit: BoxFit.cover,
+          ),
         ),
         child: Container(
           color: Colors.black.withOpacity(0.75),
@@ -159,7 +163,10 @@ class _LoginScreenState extends State<LoginScreen> {
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
                           border: Border.all(color: Colors.red, width: 3),
-                          image: const DecorationImage(image: AssetImage("assets/avatar.jpg"), fit: BoxFit.cover),
+                          image: const DecorationImage(
+                            image: AssetImage("assets/icon.png"), // اسم صحيح
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
                       const SizedBox(height: 15),
@@ -212,7 +219,7 @@ class _LoginScreenState extends State<LoginScreen> {
                             style: const TextStyle(color: Colors.white, fontSize: 22),
                             decoration: InputDecoration(
                               filled: true, fillColor: Colors.black54,
-                              hintText: isXtream ? 'رابط السيرفر http://...' : 'رابط M3U http://...',
+                              hintText: isXtream ? 'رابط السيرفر' : 'رابط M3U',
                               hintStyle: const TextStyle(color: Colors.white54),
                               enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(15), borderSide: BorderSide(color: hasFocus ? Colors.red : Colors.white54, width: 2)),
                               focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(15), borderSide: const BorderSide(color: Colors.red, width: 3)),
@@ -290,19 +297,14 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 }
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
-          image: DecorationImage(image: AssetImage("assets/bg.jpg"), fit: BoxFit.cover),
+          image: DecorationImage(image: AssetImage("assets/background.jpeg"), fit: BoxFit.cover),
         ),
         child: Container(
           color: Colors.black.withOpacity(0.75),
@@ -317,11 +319,11 @@ class _HomeScreenState extends State<HomeScreen> {
                   const Text('Service Provider', style: TextStyle(fontSize: 22, color: Colors.white70)),
                   const SizedBox(height: 80),
                   Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                    _buildBtn('LIVE TV', Icons.live_tv, true, () => _openPlayer()),
+                    _btn('LIVE TV', Icons.live_tv, true),
                     const SizedBox(width: 40),
-                    _buildBtn('SERIES', Icons.tv, false, () {}),
+                    _btn('SERIES', Icons.tv, false),
                     const SizedBox(width: 40),
-                    _buildBtn('FILMS', Icons.movie, false, () {}),
+                    _btn('FILMS', Icons.movie, false),
                   ]),
                 ],
               ),
@@ -332,65 +334,27 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  _openPlayer() {
-    Navigator.push(context, MaterialPageRoute(builder: (_) => const PlayerScreen()));
-  }
-
-  Widget _buildBtn(String title, IconData icon, bool autofocus, VoidCallback onTap) {
+  Widget _btn(String title, IconData icon, bool autofocus) {
     return Focus(
       autofocus: autofocus,
       child: Builder(builder: (context) {
         final hasFocus = Focus.of(context).hasFocus;
-        return GestureDetector(
-          onTap: onTap,
-          child: AnimatedContainer(
-            duration: const Duration(milliseconds: 200),
-            width: 300, height: 180,
-            decoration: BoxDecoration(
-              color: hasFocus ? Colors.red : Colors.grey.shade900.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(25),
-              border: Border.all(color: hasFocus ? Colors.white : Colors.white30, width: hasFocus ? 5 : 2),
-              boxShadow: hasFocus ? [BoxShadow(color: Colors.red.withOpacity(0.6), blurRadius: 25, spreadRadius: 5)] : [],
-            ),
-            child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Icon(icon, size: 70, color: Colors.white),
-              const SizedBox(height: 20),
-              Text(title, style: const TextStyle(fontSize: 34, fontWeight: FontWeight.bold)),
-            ]),
+        return AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          width: 300, height: 180,
+          decoration: BoxDecoration(
+            color: hasFocus ? Colors.red : Colors.grey.shade900.withOpacity(0.8),
+            borderRadius: BorderRadius.circular(25),
+            border: Border.all(color: hasFocus ? Colors.white : Colors.white30, width: hasFocus ? 5 : 2),
+            boxShadow: hasFocus ? [BoxShadow(color: Colors.red.withOpacity(0.6), blurRadius: 25, spreadRadius: 5)] : [],
           ),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Icon(icon, size: 70, color: Colors.white),
+            const SizedBox(height: 20),
+            Text(title, style: const TextStyle(fontSize: 34, fontWeight: FontWeight.bold)),
+          ]),
         );
       }),
-    );
-  }
-}
-
-class PlayerScreen extends StatefulWidget {
-  const PlayerScreen({super.key});
-  @override
-  State<PlayerScreen> createState() => _PlayerScreenState();
-}
-
-class _PlayerScreenState extends State<PlayerScreen> {
-  late BetterPlayerController _controller;
-  
-  @override
-  void initState() {
-    super.initState();
-    BetterPlayerDataSource dataSource = BetterPlayerDataSource(
-      BetterPlayerDataSourceType.network,
-      "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8", // Test stream
-    );
-    _controller = BetterPlayerController(
-      const BetterPlayerConfiguration(autoPlay: true, fit: BoxFit.contain),
-      betterPlayerDataSource: dataSource,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(child: BetterPlayer(controller: _controller)),
     );
   }
 }
